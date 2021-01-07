75aa22be9cb88b2b43fb31f1738a13dd6d0ba4bb
chris artrip <christopherartrip@gmail.com>
2021-01-07 18:51:59 -0500

    put each assignment in its own file,

---
+++ assignment4/assignment4.js
@@ -0,0 +1,14 @@
+const grabButton1 = document.querySelector("#btn1");
+const grabButton2 = document.querySelector("#btn2");
+let pText = document.createElement("p");
+document.body.append(pText);
+grabButton1.addEventListener("click", event => {
+    pText.innerHTML = "I'm right"
+    
+})
+
+grabButton2.addEventListener("click", event => {
+    pText.innerHTML = "No, I'm right"
+    
+})
+
