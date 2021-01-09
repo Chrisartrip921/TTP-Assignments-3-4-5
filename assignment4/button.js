47f818c283fedeae9cdb5a4644213628d29cb494
chris artrip <christopherartrip@gmail.com>
2021-01-09 11:26:36 -0500

    everything works now! :)

--- assignment4/button.js
+++ assignment4/button.js
@@ -3,9 +3,15 @@ const grabButton2 = document.querySelector("#btn2");
 let pText = document.createElement("p");
 document.body.append(pText);
 grabButton1.addEventListener("click", event => {
+    pText.classList.add("container");
+    pText.classList.add("mt-2");
+    pText.style.textAlign = "left"
     pText.innerHTML = "I'm right"
 })
 
 grabButton2.addEventListener("click", event => {
+    pText.classList.add("container");
+    pText.classList.add("mt-2");
+    pText.style.textAlign = "right"
     pText.innerHTML = "No, I'm right"
 })
